36ffcf60e4ef568cd761a70c58b484ce0130ef64
Emil Widlund <hello@emilwidlund.com>
2023-12-04 19:27:26 +0100

    clients/landing: new hero

--- clients/apps/web/src/app/ClientPage.tsx
+++ clients/apps/web/src/app/ClientPage.tsx
@@ -1,60 +1,56 @@
 'use client'
 
-import { Variants, motion, useAnimationControls } from 'framer-motion'
+import { ArrowForward } from '@mui/icons-material'
+import { motion } from 'framer-motion'
 import { LogoIcon } from 'polarkit/components/brand'
-import { useEffect, useRef } from 'react'
-
-const variants: Variants = {
-  initial: {
-    height: '100vh',
-    width: '100%',
-    top: 0,
-    borderRadius: 0,
-  },
-  minimized: {
-    height: 200,
-    width: 'calc(100% * .8)',
-    top: 'calc(100vh * .1)',
-    borderRadius: 200,
-    transition: {
-      type: 'spring',
-      damping: 20,
-      stiffness: 100,
-    },
-  },
-}
+import { useRef } from 'react'
 
 const ClientPage = () => {
   const ref = useRef(null)
-  const controls = useAnimationControls()
-
-  useEffect(() => {
-    let scrollStart = false
-
-    window.onscroll = function (e) {
-      if (window.scrollY > 100 && !scrollStart) {
-        scrollStart = true
-        controls.start('minimized')
-      }
-    }
-  }, [controls])
 
   return (
-    <main className="flex flex-col items-center">
+    <main className="bg-polar-950 flex flex-col text-white">
       <motion.section
         ref={ref}
-        variants={variants}
-        initial="initial"
-        animate={controls}
-        className="fixed flex h-screen w-full flex-col items-center justify-center bg-blue-500 p-24"
+        className="relative flex h-screen w-full flex-col justify-between p-32"
       >
-        <LogoIcon className="absolute top-24 mx-auto h-24 w-24" />
+        <header className="bg-polar-950 fixed left-0 right-0 top-0 flex w-full flex-row items-center justify-between px-32 py-24">
+          <div className="flex flex-row items-center gap-x-24">
+            <LogoIcon className="h-16 w-16" />
+            <ul className="flex flex-row gap-x-12 text-xl font-light">
+              <li>Backer</li>
+              <li>Maintainer</li>
+              <li>Community</li>
+              <li>Company</li>
+            </ul>
+          </div>
+          <div className="flex flex-row items-center gap-x-24">
+            <ul className="flex flex-row gap-x-12 text-xl font-light">
+              <li>Maintainer</li>
+              <li>Log in</li>
+            </ul>
+          </div>
+        </header>
+        <div className="flex flex-grow flex-col items-start justify-end gap-y-32">
+          <h1 className="text-[calc(100vw_/_20)] !font-normal leading-snug tracking-tight">
+            Welcome to the
+            <br />
+            Open Source Revolution
+          </h1>
+          <p className="text-3xl font-light">
+            Turn your coding endeavors into a sustainable income
+          </p>
+          <div className="flex flex-row items-center gap-x-12">
+            <button className="flex flex-row items-center gap-x-4 rounded-full bg-blue-500 px-8 py-4 font-light text-white transition-colors hover:bg-blue-400 md:text-xl">
+              <span>Get Started</span>
+              <ArrowForward />
+            </button>
 
-        <h1 className="absolute w-2/3 text-center text-7xl !font-normal leading-snug">
-          Shaping the future of
-          <br />
-          Open Source communities
-        </h1>
+            <button className="bg-transparent py-4 font-light md:text-xl">
+              <span>Open Source on GitHub</span>
+            </button>
+          </div>
+        </div>
       </motion.section>
       <motion.section className="h-screen w-full"></motion.section>
       <motion.section className="h-screen w-full"></motion.section>

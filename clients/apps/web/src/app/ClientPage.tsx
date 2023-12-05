68dd032f683d9ace3b237dbc5ea0d345338db7ea
Emil Widlund <hello@emilwidlund.com>
2023-12-05 10:29:03 +0100

    clients/landing: add curtain

--- clients/apps/web/src/app/ClientPage.tsx
+++ clients/apps/web/src/app/ClientPage.tsx
@@ -12,6 +12,7 @@ import { ArrowForward } from '@mui/icons-material'
 import { EffectComposer, Noise } from '@react-three/postprocessing'
 import {
   HTMLMotionProps,
+  Variants,
   motion,
   useMotionValue,
   useTransform,
@@ -35,13 +36,52 @@ const ClientPage = () => {
           <Noise opacity={1} />
         </EffectComposer>
       </GlobalCanvas>
-      <SmoothScrollbar config={{ duration: 0.1 }} />
+      <SmoothScrollbar config={{ duration: 0.2 }} />
+      <motion.div className="z-100 fixed inset-0 flex flex-row">
+        {Array.from({ length: 5 }).map((_, i) => {
+          const distanceFromMiddle = Math.abs(2 - i)
+          const blues = [
+            'bg-blue-950',
+            'bg-blue-700',
+            'bg-blue-500',
+            'bg-blue-700',
+            'bg-blue-950',
+          ]
+          const blue = blues[i]
+          return (
+            <motion.div
+              className={`z-100 h-full w-1/5 ${blue}`}
+              variants={{
+                initial: { y: '-100%' },
+                animate: {
+                  y: '100%',
+                  transition: {
+                    duration: 1,
+                    ease: [0.83, 0, 0.17, 1],
+                    delay: distanceFromMiddle * 0.05,
+                  },
+                },
+              }}
+              initial="initial"
+              animate="animate"
+            />
+          )
+        })}
+      </motion.div>
       <main className="flex flex-col bg-black text-white">
         <motion.section
           ref={ref}
           className="relative flex h-screen w-full flex-col justify-between p-32"
         >
-          <header className="fixed left-0 right-0 top-0 z-50 flex w-full flex-row items-center justify-between bg-black px-32 py-16">
+          <motion.header
+            className="fixed left-0 right-0 top-0 z-50 flex w-full flex-row items-center justify-between bg-black px-32 py-16"
+            variants={{
+              initial: { opacity: 0 },
+              animate: { opacity: 1, transition: { delay: 1.5, duration: 1 } },
+            }}
+            initial="initial"
+            animate="animate"
+          >
             <div className="flex flex-row items-center gap-x-24">
               <Link href="/">
                 <LogoIcon className="h-16 w-16" />
@@ -58,16 +98,37 @@ const ClientPage = () => {
                 <Link href="/login">Log in</Link>
               </ul>
             </div>
-          </header>
-          <div className="flex flex-grow flex-col items-start justify-end gap-y-16 lg:gap-y-24">
+          </motion.header>
+          <motion.div
+            className="flex flex-grow flex-col items-start justify-end gap-y-16 lg:gap-y-24"
+            variants={{
+              initial: { opacity: 1 },
+              animate: { opacity: 1, transition: { staggerChildren: 0.2 } },
+            }}
+            initial="initial"
+            animate="animate"
+          >
             <h1 className="text-5xl !font-light leading-snug tracking-tight lg:text-[calc(100vw_/_20)]">
-              Welcome to the <br className="hidden lg:block" />
-              Open Source Revolution
+              <TextReveal>
+                Welcome to the <br className="hidden lg:block" />
+              </TextReveal>
+              <TextReveal>Open Source Revolution</TextReveal>
             </h1>
-            <p className="text-xl font-light md:text-3xl">
-              Turn your coding endeavors into a sustainable income
-            </p>
-            <div className="flex flex-row items-center gap-x-12">
+            <TextReveal>
+              <p className="text-xl font-light md:text-3xl">
+                Turn your coding endeavors into a sustainable income
+              </p>
+            </TextReveal>
+            <motion.div
+              className="flex flex-row items-center gap-x-12"
+              variants={{
+                initial: { opacity: 0 },
+                animate: {
+                  opacity: 1,
+                  transition: { delay: 1.2, duration: 1 },
+                },
+              }}
+            >
               <button className="flex flex-row items-center gap-x-4 whitespace-nowrap rounded-full bg-blue-500 px-8 py-4 font-light text-white transition-colors hover:bg-white hover:text-blue-500 md:text-xl">
                 <span>Get Started</span>
                 <ArrowForward />
@@ -76,8 +137,8 @@ const ClientPage = () => {
               <button className="whitespace-nowrap bg-transparent py-4 font-light md:text-xl">
                 <span>Open Source on GitHub</span>
               </button>
-            </div>
-          </div>
+            </motion.div>
+          </motion.div>
         </motion.section>
         <motion.section className="flex w-full flex-col gap-y-12 bg-blue-500 py-16">
           <HorizontalMarquee direction="left" />
@@ -153,4 +214,25 @@ export function useTrackerMotionValue(tracker: Tracker, prop = 'progress') {
   }, [progress, scrollState, prop, onScroll, rect])
 
   return progress
+}
+
+const variants: Variants = {
+  initial: {
+    y: '100%',
+  },
+  animate: {
+    y: '0%',
+    transition: {
+      duration: 1.5,
+      ease: [0.87, 0, 0.13, 1],
+    },
+  },
+}
+
+const TextReveal = ({ children, ...props }: HTMLMotionProps<'div'>) => {
+  return (
+    <motion.div {...props} className="relative h-fit overflow-hidden">
+      <motion.div variants={variants}>{children}</motion.div>
+    </motion.div>
+  )
 }

8174899762be8ed041119f671c77afc78d624568
Emil Widlund <hello@emilwidlund.com>
2024-01-24 15:19:38 +0100

    clients/feed: add mocked creators

--- clients/apps/web/src/app/(backer)/feed/page.tsx
+++ clients/apps/web/src/app/(backer)/feed/page.tsx
@@ -1,4 +1,29 @@
 import { Feed } from '@/components/Feed/Feed'
+import Link from 'next/link'
+import { Avatar, ShadowBoxOnMd } from 'polarkit/components/ui/atoms'
+
+const featuredCreators = [
+  {
+    name: 'Kludex',
+    avatarUrl: 'https://avatars.githubusercontent.com/u/7353520?v=4',
+  },
+  {
+    name: 'emilwidlund',
+    avatarUrl: 'https://avatars.githubusercontent.com/u/10053249?v=4',
+  },
+  {
+    name: 'Sparckles',
+    avatarUrl: 'https://avatars.githubusercontent.com/u/123258275?v=4',
+  },
+  {
+    name: 'fief-dev',
+    avatarUrl: 'https://avatars.githubusercontent.com/u/97037414?v=4',
+  },
+  {
+    name: 'tuist',
+    avatarUrl: 'https://avatars.githubusercontent.com/u/38419084?v=4',
+  },
+]
 
 export default async function Page() {
   return (
@@ -6,6 +31,18 @@ export default async function Page() {
       <div className="flex w-full max-w-xl flex-col gap-y-8 pb-12">
         <Feed />
       </div>
+      <ShadowBoxOnMd>
+        <div className="flex flex-col gap-y-2">
+          <h3 className="text-lg">Popular Creators</h3>
+          <div className="flex flex-col">
+            {featuredCreators.map((creator) => (
+              <Link key={creator.name} href={`/${creator.name}`}>
+                <Avatar avatar_url={creator.avatarUrl} name={creator.name} />
+              </Link>
+            ))}
+          </div>
+        </div>
+      </ShadowBoxOnMd>
     </div>
   )
 }
